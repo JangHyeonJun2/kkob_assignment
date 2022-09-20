61a1b6a446a624b1c7618f31a8953a3100d79c46
JangHyeonJun2 <epfvkdlxj@naver.com>
2022-09-21 04:49:39 +0900

    create: User entity

---
+++ src/main/java/com/kkob/assignment/domain/User.java
@@ -0,0 +1,28 @@
+package com.kkob.assignment.domain;
+
+import com.kkob.assignment.enums.Gender;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String registrationNumber;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private String phoneNumber;
+
+    @Embedded
+    private Address address;
+}
---
+++ src/main/java/com/kkob/assignment/enums/Gender.java
@@ -0,0 +1,5 @@
+package com.kkob.assignment.enums;
+
+public enum Gender {
+    MALE, FEMALE
+}
