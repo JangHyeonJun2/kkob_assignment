894e57f5a39b4a1d89d5892701599d0c6f5633bb
JangHyeonJun2 <epfvkdlxj@naver.com>
2022-09-21 20:18:01 +0900

    fix: 불필요한 import 제거

--- src/main/java/com/kkob/assignment/repository/AccountRepository.java
+++ src/main/java/com/kkob/assignment/repository/AccountRepository.java
@@ -3,10 +3,8 @@ package com.kkob.assignment.repository;
 import com.kkob.assignment.domain.Account;
 import com.kkob.assignment.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.LockModeType;
 import java.util.Optional;
 
 @Repository
